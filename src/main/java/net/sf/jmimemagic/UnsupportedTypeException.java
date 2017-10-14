d6dd3092cc2ab4c545f54a53a14559efd546d9d8
David Castro <arimus@gmail.com>
2017-10-13 20:30:27 -0700

    remove references to the LGPL from the source code

--- src/main/java/net/sf/jmimemagic/UnsupportedTypeException.java
+++ src/main/java/net/sf/jmimemagic/UnsupportedTypeException.java
@@ -1,24 +1,6 @@
 /*
-jMimeMagic(TM) is a Java library for determining the content type of files or
-streams.
-
-Copyright (C) 2004 David Castro
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-For more information, please email arimus@users.sourceforge.net
+jMimeMagic (TM) is a Java Library for determining the content type of files or streams
+Copyright (C) 2003-2017 David Castro
 */
 package net.sf.jmimemagic;
 
